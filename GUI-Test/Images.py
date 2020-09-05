dc768673c7ae3c9050c47e174ce01d767b070f18
rgkog <rgkog@DESKTOP-GI2KK2V>
2020-09-05 23:10:23 +0300

    Image

--- GUI-Test/Images.py
+++ GUI-Test/Images.py
@@ -1,5 +1,33 @@
+  
 from tkinter import *
-from PIL import ImageTk, Image
+from PIL import ImageTk,Image
 
-root=Tk()
-root.title("IMAGES GUI")
+root = Tk()
+root.title('Learn To Code at Codemy.com')
+#root.iconbitmap('E:/Python/FinalProject/Final-Project-SCE/Test/website.jpg')
+
+
+my_img = ImageTk.PhotoImage(Image.open("E:/Python/FinalProject/Final-Project-SCE/Test/website.jpg"))
+my_label = Label(image=my_img)
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.pack()
+
+
+
+
+
+
+root.mainloop()
