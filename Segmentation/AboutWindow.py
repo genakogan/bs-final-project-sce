55ba1bdd8ba93adc5b526b6e53e86e25fb8013b8
Genady Kogan <rgkogan@gmail.com>
2020-10-10 19:30:54 +0300

    new window

--- Segmentation/AboutWindow.py
+++ Segmentation/AboutWindow.py
@@ -1,21 +1,48 @@
-import tkinter as tk
-class abWindow():
-    root = tk.Tk()
-    S = tk.Scrollbar(root)
-    T = tk.Text(root, height=4, width=50)
-    S.pack(side=tk.RIGHT, fill=tk.Y)
-    T.pack(side=tk.LEFT, fill=tk.Y)
-    S.config(command=T.yview)
-    T.config(yscrollcommand=S.set)
-    quote = """HAMLET: To be, or not to be--that is the question:
-Whether 'tis nobler in the mind to suffer
-The slings and arrows of outrageous fortune
-Or to take arms against a sea of troubles
-And by opposing end them. To die, to sleep--
-No more--and by a sleep to say we end
-The heartache, and the thousand natural shocks
-That flesh is heir to. 'Tis a consummation
-Devoutly to be wished."""
-    T.insert(tk.END, quote)
-    tk.mainloop()
-    
+# This will import all the widgets 
+# and modules which are available in 
+# tkinter and ttk module 
+from tkinter import * 
+from tkinter.ttk import *
+  
+# creates a Tk() object 
+master = Tk() 
+  
+# sets the geometry of main  
+# root window 
+master.geometry("200x200") 
+  
+  
+# function to open a new window  
+# on a button click 
+def openNewWindow(): 
+      
+    # Toplevel object which will  
+    # be treated as a new window 
+    newWindow = Toplevel(master) 
+  
+    # sets the title of the 
+    # Toplevel widget 
+    newWindow.title("New Window") 
+  
+    # sets the geometry of toplevel 
+    newWindow.geometry("200x200") 
+  
+    # A Label widget to show in toplevel 
+    Label(newWindow,  
+          text ="This is a new window").pack() 
+  
+  
+label = Label(master,  
+              text ="This is the main window") 
+  
+label.pack(pady = 10) 
+  
+# a button widget which will open a  
+# new window on button click 
+btn = Button(master,  
+             text ="Click to open a new window",  
+             command = openNewWindow) 
+btn.pack(pady = 10) 
+  
+# mainloop, runs infinitely 
+mainloop()
