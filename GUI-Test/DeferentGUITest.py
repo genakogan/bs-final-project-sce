bad54b6cb34b6ab69b44c19404d4246e002fc7e0
Genady Kogan <rgkogan@gmail.com>
2020-10-13 12:49:11 +0300

    stile menu
    Signed-off-by: Genady Kogan <rgkogan@gmail.com>

--- GUI-Test/DeferentGUITest.py
+++ GUI-Test/DeferentGUITest.py
@@ -1,67 +1,86 @@
-from PyQt5.QtWidgets import * 
-from PyQt5 import QtCore, QtGui 
-from PyQt5.QtGui import * 
-from PyQt5.QtCore import * 
-import sys 
-  
-# QCalendarWidget Class 
-class Calendar(QCalendarWidget): 
-  
-    # constructor 
-    def __init__(self, parent = None): 
-        super(Calendar, self).__init__(parent) 
-  
-  
-  
-class Window(QMainWindow): 
-  
-    def __init__(self): 
-        super().__init__() 
-  
-        # setting title 
-        self.setWindowTitle("Python ") 
-  
-        # setting geometry 
-        self.setGeometry(100, 100, 500, 400) 
-  
-        # calling method 
-        self.UiComponents() 
-  
-        # showing all the widgets 
-        self.show() 
-  
-  
-    # method for components 
-    def UiComponents(self): 
-  
-        # creating a QCalendarWidget object 
-        # as Calendar class inherits QCalendarWidget 
-        self.calendar = Calendar(self) 
-  
-        # setting cursor 
-        self.calendar.setCursor(Qt.PointingHandCursor) 
-  
-        # setting size of the calendar 
-        self.calendar.resize(350, 240) 
-  
-        # move the calendar 
-        self.calendar.move(10, 10) 
-  
-        # setting stylesheet 
-        # adding background color to the calendar's menu 
-        self.calendar.setStyleSheet("QCalendarWidget QMenu"
-                                    "{"
-                                    "background-color : lightgreen;"
-                                    "}"
-                                    ) 
-  
-  
-  
-# create pyqt5 app 
-App = QApplication(sys.argv) 
-  
-# create the instance of our Window 
-window = Window() 
-  
-# start the app 
-sys.exit(App.exec())
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore    import *
+from PyQt5.QtGui     import *
+
+
+qss = """
+QMenuBar {
+    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,
+                                      stop:0 lightgray, stop:1 darkgray);
+}
+QMenuBar::item {
+    spacing: 3px;           
+    padding: 2px 10px;
+    background-color: rgb(210,105,30);
+    color: rgb(255,255,255);  
+    border-radius: 5px;
+}
+QMenuBar::item:selected {    
+    background-color: rgb(244,164,96);
+}
+QMenuBar::item:pressed {
+    background: rgb(128,0,0);
+}
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */  
+
+QMenu {
+    background-color: #ABABAB;   
+    border: 1px solid black;
+    margin: 2px;
+}
+QMenu::item {
+    background-color: transparent;
+}
+QMenu::item:selected { 
+    background-color: #654321;
+    color: rgb(255,255,255);
+}
+"""
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+### vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv       
+        mainMenu = self.menuBar()
+        testMenu = mainMenu.addMenu('mainMenu') 
+
+        test1_dropButton = QAction('Test 1', self)
+        testMenu.addAction(test1_dropButton)
+
+        test2_dropButton = QAction('Test 2', self)
+        test2_dropButton.triggered.connect(self.displayImage)
+        testMenu.addAction(test2_dropButton)
+
+        test_dropButton = QAction('Exit', self)
+        test_dropButton.setShortcut('Ctrl+E')
+        test_dropButton.triggered.connect(self.close)
+        testMenu.addAction(test_dropButton)         
+### ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        self.layV = QVBoxLayout(central_widget)               
+
+    def displayImage(self):
+        pixmap = QPixmap("D:/_Qt/img/pyqt.jpg") 
+        lbl    = QLabel(self)
+        self.layV.addWidget(lbl)
+        lbl.setPixmap(pixmap.scaled(400, 400, Qt.KeepAspectRatio))        
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    app.setStyleSheet(qss)         # <--------------------------------
+
+    ex = Example()
+    #ex.setWindowTitle('Style Sheet: QMenuBar, QMenu')
+    ex.setWindowIcon(QIcon('D:/_Qt/img/py-qt.png')) 
+    ex.resize(420,440)
+    ex.show()
+    sys.exit(app.exec_())
