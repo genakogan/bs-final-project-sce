5e559ef594c4640422bb02b486f969b4fa70704f
Genady Kogan <rgkogan@gmail.com>
2020-10-13 14:09:46 +0300

    paint
    Signed-off-by: Genady Kogan <rgkogan@gmail.com>

--- Segmentation/Paint.py
+++ Segmentation/Paint.py
@@ -42,6 +42,42 @@ QMenu::item:selected {
     color: rgb(255,255,255);
 }
 """
+
+
+qbb = """
+QMenuBar {
+    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,
+                                      stop:0 lightgray, stop:1 darkgray);
+}
+QMenuBar::item {
+    spacing: 3px;           
+    padding: 2px 10px;
+    background-color: rgb(176, 244, 98);
+    color: rgb(255,255,255);  
+    border-radius: 5px;
+}
+QMenuBar::item:selected {    
+    background-color: rgb(176, 244, 98);
+}
+QMenuBar::item:pressed {
+    background: rgb(128,0,0);
+}
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */  
+
+QMenu {
+    background-color: #ABABAB;   
+    border: 1px solid black;
+    margin: 2px;
+}
+QMenu::item {
+    background-color: transparent;
+}
+QMenu::item:selected { 
+    background-color: #654321;
+    color: rgb(176, 244, 98);;
+}
+"""
 # window class 
 class Window(QMainWindow):
     
@@ -93,7 +129,20 @@ class Window(QMainWindow):
         
         # creating file menu for save and clear action 
         fileMenu = mainMenu.addMenu("File")
-        
+        fileMenu.setStyleSheet(
+            """
+            QMenu
+            {
+                font: 18pt;
+                background-color: purple;
+            }
+
+            QMenu::item:selected
+            {
+                background-color: green
+            }
+            """
+            )
         # creating edit menu for undo and redo
         editMenu = mainMenu.addMenu("Edit") 
   
@@ -308,7 +357,7 @@ class Window(QMainWindow):
   
     # method for clearing every thing on canvas 
     def clear(self): 
-        
+        App.setStyleSheet(qbb)
         # make the whole canvas white 
         self.imageDraw = QtGui.QImage(self.name)
         
